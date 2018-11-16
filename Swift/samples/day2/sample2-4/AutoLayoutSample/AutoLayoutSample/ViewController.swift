243f02dbffd6604cf24a8033612c660800679e1a
Hideki Matsuoka <matsuoka.hide@gmail.com>
2018-11-16 11:26:07 +0900

    fix sample sample2-4/AutoLayoutSample

--- Swift/samples/day2/sample2-4/AutoLayoutSample/AutoLayoutSample/ViewController.swift
+++ Swift/samples/day2/sample2-4/AutoLayoutSample/AutoLayoutSample/ViewController.swift
@@ -20,19 +20,15 @@ class ViewController: UIViewController {
         greenView.backgroundColor = .green
         greenView.translatesAutoresizingMaskIntoConstraints = false
         brownView.addSubview(greenView)
-        brownView.addConstraints([
-            NSLayoutConstraint(item: greenView, attribute: .top, relatedBy: .equal, toItem: brownView, attribute: .top, multiplier: 1, constant: 50),
-            NSLayoutConstraint(item: greenView, attribute: .left, relatedBy: .equal, toItem: brownView, attribute: .left, multiplier: 1, constant: 50),
-            NSLayoutConstraint(item: greenView, attribute: .right, relatedBy: .equal, toItem: brownView, attribute: .right, multiplier: 1, constant: -50),
-            NSLayoutConstraint(item: greenView, attribute: .bottom, relatedBy: .equal, toItem: brownView, attribute: .bottom, multiplier: 1, constant: -50)
-        ])
+
+        greenView.topAnchor.constraint(equalTo: brownView.topAnchor, constant: 50).isActive = true
+        greenView.leadingAnchor.constraint(equalTo: brownView.leadingAnchor, constant: 50).isActive = true
+        greenView.trailingAnchor.constraint(equalTo: brownView.trailingAnchor, constant: -50).isActive = true
+        greenView.bottomAnchor.constraint(equalTo: brownView.bottomAnchor, constant: -50).isActive = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
