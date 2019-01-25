66aefa59012be1a9725c409dcf6461406c43f526
marty-suzuki <s1180183@gmail.com>
2019-01-25 14:51:29 +0900

    fix dya3 4.3

---
+++ Swift/after/day3/4.3/WebViewSample/WebViewSample/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  WebViewSample
+//
+//  Created by 鈴木大貴 on 2016/10/30.
+//  Copyright © 2016年 marty-suzuki. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        self.window = {
+            let window = UIWindow(frame: UIScreen.main.bounds)
+            window.rootViewController = UINavigationController(rootViewController: ViewController())
+            window.makeKeyAndVisible()
+            return window
+        }()
+
+        return true
+    }
+}
+
--- Swift/before/day3/4.3/WebViewSample/WebViewSample/ViewController.swift
+++ Swift/before/day3/4.3/WebViewSample/WebViewSample/ViewController.swift
@@ -15,12 +15,21 @@ class ViewController: UIViewController {
     
     // TODO: ボタン変数を追加
 
+    private var observations: [NSKeyValueObservation] = []
+
+    deinit {
+        observations.forEach {
+            $0.invalidate()
+        }
+        observations.removeAll()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         webView.navigationDelegate = self
+        webView.allowsBackForwardNavigationGestures = true
         view.addSubview(webView)
         webView.translatesAutoresizingMaskIntoConstraints = false
         view.addConstraints([
@@ -30,24 +39,28 @@ class ViewController: UIViewController {
             NSLayoutConstraint(item: webView, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1, constant: 0)
         ])
         
-        guard let url = URL(string: "https://abematimes.com/pages/381282/archives") else { return }
+        guard let url = URL(string: "https://github.com/apple") else { return }
         let request = URLRequest(url: url)
         webView.load(request)
         
         // TODO: toolbarにボタンを追加
-        
-        // TODO: KVOでwebView.canGoBack, webView.canGoForwardを監視
-    }
+        navigationController?.isToolbarHidden = false
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // TODO: KVOでwebView.canGoBack, webView.canGoForwardを監視
+
+        observations = []
     }
     
     // TODO: toolbarのボタンが押された時の処理
-    
 }
 
 extension ViewController: WKNavigationDelegate {
-    // TODO: 読み込み完了のデリゲートメソッド追加、タイトルの変更など
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        print(navigationAction.request.url)
+        decisionHandler(.allow)
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("didFinish")
+    }
 }
